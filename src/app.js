aad867c308c3f5e3e36eb8ffa932b5f452c0a465
Edson Perotoni <perotoni@gmail.com>
2020-04-28 21:04:54 -0300

    rotas POST GET PUT

--- src/app.js
+++ src/app.js
@@ -1,7 +1,6 @@
 const express = require("express");
 const cors = require("cors");
-
-// const { uuid } = require("uuidv4");
+const { uuid,isUuid } = require('uuidv4');
 
 const app = express();
 
@@ -10,16 +9,62 @@ app.use(cors());
 
 const repositories = [];
 
+function logRequests(request, response, next){
+  const {method, url} = request;
+  const logLabel = `[${method.toUpperCase()}] ${url}`;
+  console.time(logLabel);
+  next();
+  console.timeEnd(logLabel);
+}
+
+function validateRepositorieId(request,response,next){
+    const {id} = request.params;
+    if(!isUuid(id)){
+      return response.status(400).json({error:"Invalid uuid"})
+    }
+    const repositorieIndex = repositories.findIndex(repository => repository.id == id);
+    if(repositorieIndex<0){
+      return response.status(400).json({error:"repository not found"})
+    }
+    next();
+}
+
+app.use(logRequests);
+app.use('/repositories/:id', validateRepositorieId);
+
 app.get("/repositories", (request, response) => {
-  // TODO
+  return response.status(200).json(
+    repositories 
+  );
 });
 
 app.post("/repositories", (request, response) => {
-  // TODO
+  const {url,title,techs} = request.body;
+  const repository = {id:uuid(),title,url,techs,likes:0};
+  repositories.push(repository);
+  return response.status(200).json(
+    repositories[repositories.length-1]
+  );
 });
 
 app.put("/repositories/:id", (request, response) => {
-  // TODO
+  const {id} = request.params;
+  const {title,url,techs} = request.body;
+  const repositorieIndex = repositories.findIndex(repository=>repository.id === id);
+  
+  const repository = {
+    id,
+    title,
+    url,
+    techs,
+    likes: repositories[repositorieIndex].likes 
+  }
+
+  repositories[repositorieIndex] =  repository;
+  
+  return response.json(
+    repositories[repositorieIndex]
+  );
 });
 
 app.delete("/repositories/:id", (request, response) => {
